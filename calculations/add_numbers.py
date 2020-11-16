1cb9b1c5570f228f04a0503bfdebe903410a92de
Espen Gunnarsen <espen.gunnarsen@byr.oslo.kommune.no>
2020-11-16 07:03:55 +0100

    Update tox

--- calculations/add_numbers.py
+++ calculations/add_numbers.py
@@ -2,4 +2,3 @@ def add_numbers(x1: int, x2: int):
     """Add the two numbers and return the result."""
 
     return x1 + x2
-
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -4,4 +4,3 @@ from calculations.add_numbers import add_numbers
 def test_add_numbers():
 
     assert add_numbers(2, 4) == 6
-
