663a4f297d62cb0f1e8c9d9a7e63e8a7b7b57b61
Espen Gunnarsen <espen.gunnarsen@byr.oslo.kommune.no>
2020-11-18 07:29:39 +0100

    Wrapping up tox

--- calculations/add_numbers.py
+++ calculations/add_numbers.py
@@ -1,4 +1,14 @@
-def add_numbers(x1: int, x2: int):
-    """Add the two numbers and return the result."""
+import numpy as np
 
-    return x1 + x2
+def add_numbers(x1: int, x2: int) -> int:
+    """
+    Add the two numbers and return the result.
+    Use the amazing capabilities of numpy!
+    """
+
+    n1 = np.array([x1])
+    n2 = np.array([x2])
+    n = n1 + n2
+    x = int(n[0])
+    
+    return x
